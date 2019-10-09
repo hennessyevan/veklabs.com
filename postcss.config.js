0637eabbd5d99b2def5d461814b9b03d31059378
hennessyevan <hennessyevan@users.noreply.github.com>
2019-10-09 12:29:00 -0400

    remove postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,20 @@
 module.exports = {
-  plugins: [
-    require("postcss-import")({ from: "assets/css/main.css" }),
-    require("autoprefixer")(),
-    require("postcss-nesting")(),
-    require("postcss-short")(),
-    require("postcss-color-hex-alpha")(),
-    require("postcss-hexrgba")(),
-    require("postcss-custom-unit")({
-      units: [
-        {
-          // Major Scale
-          from: "maj",
-          convert: function (val) {
-            return val * 8 + "px";
-          }
-        }
-      ]
-    })
-  ]
+	plugins: [
+		require("autoprefixer")(),
+		require("postcss-nesting")(),
+		require("postcss-short")(),
+		require("postcss-color-hex-alpha")(),
+		require("postcss-hexrgba")(),
+		require("postcss-custom-unit")({
+			units: [
+				{
+					// Major Scale
+					from: "maj",
+					convert: function(val) {
+						return val * 8 + "px";
+					}
+				}
+			]
+		})
+	]
 };
