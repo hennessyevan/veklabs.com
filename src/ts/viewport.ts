2513eb37737f18c34de39e233084d92106c17048
hennessyevan <hello@hennessyevan.com>
2019-10-11 17:05:40 -0400

    add smooth scrolling

---
+++ src/ts/viewport.ts
@@ -0,0 +1,16 @@
+import { styler, inertia, spring, tween, decay } from "popmotion"
+
+const headerAnchors = document.querySelectorAll(".anchor")
+
+headerAnchors.forEach(anchor => {
+ anchor.addEventListener("click", e => {
+  e.preventDefault()
+
+  const targetEl = anchor.getAttribute("href")
+  const targetTop = document.querySelector(targetEl).getBoundingClientRect().top
+
+  tween({ from: window.pageYOffset, to: targetTop }).start(v =>
+   styler(window).set("scrollTop", v)
+  )
+ })
+})
