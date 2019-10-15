b6aac8c6c3f163f8cdf195f6b35dcae47b7afd19
hennessyevan <hello@hennessyevan.com>
2019-10-15 12:46:33 -0400

    fix team modal mobile

--- src/ts/team-modal.ts
+++ src/ts/team-modal.ts
@@ -8,28 +8,39 @@ members.forEach(member => {
  const info = infoOverlay.querySelector(".member-info")
  const overlay = styler(infoOverlay)
  const inner = styler(info)
+ let active = false
+
+ document.body.appendChild(infoOverlay)
 
  listen(member, "click").start(e => {
-  listen(infoOverlay, "click").start(e => {
-   if (e.target !== infoOverlay) {
+  function closeModal(start = 0) {
+   if (active !== true) {
     return
    }
-
+   active = false
    document.body.style.overflow = ""
 
    tween({ from: 1, to: 0 }).start(v => overlay.set("opacity", v))
    spring({
-    from: "0%",
-    to: "10%",
+    from: start,
+    to: 100,
     stiffness: 80
    }).start(v => inner.set("y", v))
    setTimeout(() => {
     overlay.set("display", "none")
    }, 300)
+  }
+
+  listen(infoOverlay, "click").start(e => {
+   if (e.target !== infoOverlay) {
+    return
+   }
+
+   closeModal()
   })
 
   overlay.set("display", "flex")
-
+  active = true
   tween({ from: 0, to: 1 }).start(v => overlay.set("opacity", v))
   spring({
    from: "50%",
@@ -39,5 +50,24 @@ members.forEach(member => {
 
   document.body.style.overflow = "hidden"
   member.setAttribute("data-active", "true")
+
+  // Swipe to Dismiss
+  let startY = 0
+  let endY = 0
+
+  function startTracking(e: TouchEvent) {
+   startY = e.changedTouches[0].screenY
+  }
+
+  function stopTracking(e: TouchEvent) {
+   endY = e.changedTouches[0].screenY
+
+   if (endY - startY >= 200) {
+    closeModal(endY)
+   }
+  }
+
+  listen(infoOverlay, "touchstart").start(startTracking)
+  listen(document, "touchend").start(stopTracking)
  })
 })
