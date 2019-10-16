5487e03eee88afe54532f3b8a13d3402d77be6c5
hennessyevan <hello@hennessyevan.com>
2019-10-16 00:47:45 -0400

    change iframes to video

--- content/videos/yellow-dot.md
+++ content/videos/yellow-dot.md
@@ -1,6 +1,8 @@
 +++
  title = 'Yellow Dot'
  videoID = "365333005"
+ videoURL = "https://player.vimeo.com/external/365333005.hd.mp4?s=78bbae21ff82495449a3d7443da90c86ce137ca2&profile_id=175"
+ previewURL = "https://player.vimeo.com/external/365333005.sd.mp4?s=6a5178d266d5db40d088323105bdf1f0f963faa6&profile_id=164"
  image = "images/7_Yellow_Dot_Thumb.jpg"
  type = "Advertisement"
 +++
--- src/ts/video-modal.ts
+++ src/ts/video-modal.ts
@@ -1,4 +1,3 @@
-import Player from "@vimeo/player"
 import { listen, styler, tween } from "popmotion"
 import "./contact"
 
@@ -7,83 +6,68 @@ const overlayEl = document.getElementById("overlay")
 const overlay = styler(overlayEl)
 
 // modals
-let player
 videos.forEach(video => {
- const videoID = video.getAttribute("data-videoID")
- if (!videoID) return
+ const videoSrc = video.getAttribute("data-url")
+ const poster = video.getAttribute("data-poster")
+ const preview: HTMLVideoElement = video.querySelector(".preview")
+ const previewStyler = preview && styler(preview)
+ if (!videoSrc) return
 
- // let preview
- // listen(video, "mouseover").start(async e => {
- // window.requestAnimationFrame(async () => {
- //  let count = 0
- //  const previewEl = video.querySelector(".preview")
- //  const previewStyler = styler(previewEl)
- //  const { height } = video.getBoundingClientRect()
+ preview.addEventListener("mouseover", function() {
+  this.play()
+  let hit = false
+  this.addEventListener("timeupdate", e => {
+   if (this.currentTime >= 15 && !hit) {
+    hit = true
+    tween({
+     from: 1,
+     to: 0,
+     duration: 1500
+    }).start(v => previewStyler.set("opacity", v))
 
- //  preview = new Player(previewEl, {
- //   id: videoID,
- //   background: true,
- //   height: height + 50,
- //   autopause: true,
- //   quality: "240p"
- //  })
+    setTimeout(() => {
+     this.currentTime = 0
+     tween({
+      from: 0,
+      to: 1,
+      duration: 500
+     }).start(v => previewStyler.set("opacity", v))
+     hit = false
+    }, 1500)
+   }
+  })
+ })
 
- //  await preview.play()
- //  await preview.setCurrentTime(10)
- //  await preview.addCuePoint(20)
-
- //  preview.on("cuepoint", async () => {
- //   if (count <= 3) {
- //    await preview.setCurrentTime(10)
- //    count++
- //   }
- //  })
-
- //  preview.on("error", console.log)
-
- //  preview.on("loaded", () =>
- //   tween({
- //    from: { opacity: 0, transform: "scale(0.9)" },
- //    to: { opacity: 1, transform: "scale(1)" },
- //    duration: 1500
- //   }).start(v => previewStyler.set(v))
- //  )
- // })
-
- // listen(video, "mouseout").start(async e => {
- //  const previewEl = video.querySelector(".preview")
- //  const previewStyler = styler(previewEl)
- //  tween({
- //   from: { opacity: 1, transform: "scale(1)" },
- //   to: { opacity: 0, transform: "scale(0.85)" },
- //   duration: 225
- //  }).start(v => previewStyler.set(v)),
- //   await preview.destroy()
- // })
+ preview.addEventListener("mouseout", function() {
+  this.pause()
+  this.currentTime = 0
+  previewStyler.set("opacity", "")
+ })
 
  listen(video, "click").start(e => {
   // Show overlay
-  overlay.set("display", "initial")
+  overlay.set("display", "flex")
   document.body.style.overflow = "hidden"
   tween({ to: { opacity: 1 }, duration: 500 }).start(v => overlay.set(v))
 
-  setTimeout(() => {
-   player = new Player("player", {
-    id: videoID,
-    autoplay: true,
-    width: window.innerWidth * 0.88,
-    playsinline: false,
-    color: "#FFFFFF"
-   })
-  }, 650)
+  const video = `
+  <div class="video-wrapper">
+   <video id="player" src="${videoSrc}" poster="${poster}" controls autoplay></video>
+  </div>
+ `
+
+  overlayEl.innerHTML = video
  })
 })
 
-function cleanUp() {
- player && player.destroy()
+overlayEl.addEventListener("click", e => {
+ cleanUp()
+})
 
+function cleanUp() {
  tween({ to: { opacity: 0 }, duration: 500 }).start(v => overlay.set(v))
  document.body.style.overflow = ""
+ overlayEl.innerHTML = ""
  setTimeout(() => {
   overlay.set("display", "none")
  }, 500)
