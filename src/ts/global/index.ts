1b062af0af97edab3827b709aba4c920d73681ac
hennessyevan <hennessyevan@users.noreply.github.com>
2020-04-21 17:19:52 -0400

    remove any lingering service workers

--- src/ts/global/index.ts
+++ src/ts/global/index.ts
@@ -1,2 +1,11 @@
 import "./header"
 import "./favicon"
+
+/**
+ * Delete all service workers from previous
+ */
+navigator.serviceWorker.getRegistrations().then(function (registrations) {
+ for (let registration of registrations) {
+  registration.unregister()
+ }
+})
