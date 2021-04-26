3a277f079a8094a5e57a0fd351f8b43e16d63101
hennessyevan <hello@hennessyevan.com>
2021-04-26 10:30:35 -0400

    Update banner config

--- assets/js/global/footer.ts
+++ assets/js/global/footer.ts
@@ -1,9 +1,10 @@
 import { chain, tween, styler, delay } from "popmotion"
 
-const banner = styler(document.querySelector(".banner"))
+const bannerEl: HTMLElement = document.querySelector(".banner")
+const banner = styler(bannerEl)
 
 chain(
- delay(10000),
+ delay(Number(bannerEl.dataset.delay)),
  tween({ from: { opacity: 1 }, to: { opacity: 0 } }),
  tween({ from: { display: "initial" }, to: { display: "none" } })
 ).start((v) => banner.set(v))
