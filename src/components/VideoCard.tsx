1eb62a514dd64735fa55d26c9d5b1260f03ae740
Evan Hennessy <git@hennessyevan.com>
2024-11-04 10:44:00 -0500

    Add initial reports #78

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -15,7 +15,13 @@ import {
   type Variants,
 } from "framer-motion"
 import { ArrowRight, Loader2 } from "lucide-react"
-import { useEffect, useRef, useState } from "react"
+import {
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+  type PropsWithChildren,
+} from "react"
 
 type VideoData = ContentEntryMap["video"][string]["data"]
 
@@ -92,14 +98,21 @@ export default function VideoCard(videoData: VideoData) {
     <MotionConfig transition={{ type: "spring" }}>
       <LayoutGroup id={title}>
         <motion.div
-          className="relative size-full z-50 aspect-video h-[269px] w-[480px] border-neutral-300 rounded-xl snap-start scroll-mx-6 cursor-pointer"
-          style={{ zIndex: popupIsShown ? 100 : hovered ? 50 : undefined }}
+          className="relative size-full aspect-video h-[269px] w-[480px] border-neutral-300 rounded-xl snap-start scroll-mx-6 cursor-pointer"
+          style={{
+            zIndex: popupIsShown
+              ? 100
+              : hovered || popupWasJustShown
+                ? 1
+                : undefined,
+          }}
           onMouseEnter={() => !popupWasJustShown && setHovered(true)}
           onMouseLeave={() => setHovered(false)}
           onClick={() => setPopupIsShown(true)}
           variants={containerVariants}
-          whileHover={!popupWasJustShown ? "hover" : undefined}
+          whileHover={!popupWasJustShown && !popupIsShown ? "hover" : undefined}
           layoutId={`video-${title}`}
+          layout
           transition={{ duration: POPUP_DURATION, type: "spring" }}
         >
           <motion.span
@@ -108,6 +121,7 @@ export default function VideoCard(videoData: VideoData) {
             initial="initial"
             animate={hovered && !popupWasJustShown ? "hover" : "initial"}
             layoutId={`video-title-${title}`}
+            layout="preserve-aspect"
             transition={{
               duration: HOVER_DURATION / 1.25,
               type: "spring",
@@ -212,7 +226,7 @@ export default function VideoCard(videoData: VideoData) {
           </AnimatePresence>
         </motion.div>
 
-        <VideoInformationPopup
+        <VideoInformationPopupWrapper
           isOpen={popupIsShown}
           onOpenChange={setPopupIsShown}
           {...videoData}
@@ -222,82 +236,137 @@ export default function VideoCard(videoData: VideoData) {
   )
 }
 
-function VideoInformationPopup({
-  previewURL,
-  image,
-  isOpen,
-  title,
-  onOpenChange,
-}: VideoData & {
+type VideoInformationPopupProps = VideoData & {
   isOpen: boolean
   onOpenChange: (isOpen: boolean) => void
-}) {
-  const { refs, floatingStyles, context } = useFloating({
-    open: isOpen,
-    onOpenChange,
-  })
+}
 
-  const dismiss = useDismiss(context)
-
-  const { getFloatingProps } = useInteractions([dismiss])
-
+function VideoInformationPopupWrapper({
+  children,
+  ...props
+}: PropsWithChildren<VideoInformationPopupProps>) {
+  const { isOpen } = props
   return (
     <FloatingPortal id="video-popup">
-      {isOpen && <FloatingOverlay id="video-popup" lockScroll />}
       <AnimatePresence mode="popLayout">
-        {isOpen && (
-          <motion.div
-            className="w-screen h-screen fixed inset-0 z-50 bg-neutral-950/80 backdrop-blur-3xl"
-            initial={{ opacity: 0, backdropFilter: "blur(0px)" }}
-            animate={{
-              opacity: 1,
-              backdropFilter: "blur(64px)",
-            }}
-            exit={{
-              opacity: 0,
-              backdropFilter: "blur(0px)",
-            }}
-            transition={{ duration: POPUP_DURATION, type: "spring" }}
-          >
-            <motion.div
-              ref={refs.setFloating}
-              {...getFloatingProps()}
-              style={floatingStyles}
-              className="w-full"
-            >
-              <motion.div className="w-container max-w-7xl p-10 mx-auto">
-                <motion.video
-                  layoutId={`video-${title}`}
-                  controls={false}
-                  className="object-cover size-full aspect-video rounded-xl shadow-2xl ring-2 ring-white/5 shadow-neutral-950"
-                  src={previewURL}
-                  poster={image}
-                  transition={{
-                    duration: POPUP_DURATION / 2,
-                    layout: { type: "spring", duration: POPUP_DURATION },
-                  }}
-                  preload="none"
-                  autoPlay
-                  playsInline
-                  onError={(e) => console.error(e)}
-                />
-                <div className="h-10" />
-                <motion.span
-                  className="absolute z-50 pointer-events-none font-semibold text-2xl text-shadow-lg shadow-neutral-950 flex items-center"
-                  layoutId={`video-title-${title}`}
-                  transition={{
-                    duration: POPUP_DURATION / 2,
-                    type: "spring",
-                    layout: { duration: POPUP_DURATION / 2 },
-                  }}
-                >
-                  {title}
-                </motion.span>
-              </motion.div>
-            </motion.div>
-          </motion.div>
-        )}
+        {isOpen ? <VideoInformationPopup {...props} /> : null}
       </AnimatePresence>
     </FloatingPortal>
   )
 }
+
+const VideoInformationPopup = forwardRef<
+  HTMLDivElement,
+  VideoInformationPopupProps
+>(({ previewURL, image, isOpen, title, type, onOpenChange }, forwardedRef) => {
+  const { refs, context } = useFloating({
+    open: true,
+    onOpenChange,
+  })
+
+  const dismiss = useDismiss(context)
+  const { getFloatingProps } = useInteractions([dismiss])
+
+  return (
+    <FloatingOverlay
+      ref={forwardedRef}
+      id="video-popup"
+      className="w-screen h-screen fixed top-0 left-0 z-50 overscroll-contain"
+      onClickCapture={() => onOpenChange(false)}
+    >
+      <motion.div
+        className="bg-neutral-950/70 overflow-y-scroll backdrop-blur-3xl backdrop-saturate-200 backdrop-brightness-200"
+        initial={{ opacity: 0, backdropFilter: "blur(0px)" }}
+        animate={{
+          opacity: 1,
+          backdropFilter: "blur(64px)",
+        }}
+        exit={{
+          opacity: 0,
+          backdropFilter: "blur(0px)",
+        }}
+        transition={{ duration: POPUP_DURATION, type: "spring" }}
+        ref={refs.setFloating}
+        {...getFloatingProps()}
+      >
+        <motion.div className="w-full">
+          <motion.div className="w-container max-w-7xl p-10 mx-auto relative">
+            <motion.video
+              layoutId={`video-${title}`}
+              controls
+              className="absolute object-cover aspect-video rounded-xl w-full shadow-lg ring-2 ring-white/5 shadow-neutral-950"
+              src={previewURL}
+              poster={image}
+              transition={{
+                duration: POPUP_DURATION / 2,
+                layout: { type: "spring", duration: POPUP_DURATION },
+              }}
+              preload="none"
+              autoPlay
+              playsInline
+              onLayoutAnimationComplete={() => {}}
+              onError={(e) => console.error(e)}
+            />
+            <motion.div className="mb-20 aspect-video w-full min-w-96" />
+            <motion.span
+              className="z-50 absolute h-5 pointer-events-none font-semibold text-2xl text-shadow-lg shadow-neutral-950 flex items-center"
+              layoutId={`video-title-${title}`}
+              transition={{
+                duration: POPUP_DURATION / 2,
+                type: "spring",
+                layout: { duration: POPUP_DURATION / 2 },
+              }}
+              layout="preserve-aspect"
+            >
+              {title}
+            </motion.span>
+            <motion.div
+              initial={{ opacity: 0, y: 25 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                delay: POPUP_DURATION - POPUP_DURATION / 5,
+                type: "spring",
+                bounce: 0.35,
+                duration: POPUP_DURATION * 2,
+                opacity: {
+                  type: "tween",
+                  duration: POPUP_DURATION * 2,
+                  delay: POPUP_DURATION - POPUP_DURATION / 5,
+                },
+              }}
+              className="pt-14 flex flex-col gap-6"
+            >
+              <div className="flex gap-5 justify-between">
+                <div className="max-w-prose flex flex-col gap-3">
+                  <span className="uppercase tracking-widest text-xs font-bold">
+                    Description
+                  </span>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </p>
+                </div>
+                <div className="flex flex-col min-h-36 gap-3 bg-neutral-200/10 rounded-xl ring-1 ring-white/15 p-6 w-56 mb-auto">
+                  {type && (
+                    <div className="flex flex-col gap-3">
+                      <span className="uppercase tracking-widest text-xs font-bold">
+                        Category
+                      </span>
+                      <span>{type}</span>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </motion.div>
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </FloatingOverlay>
+  )
+})
